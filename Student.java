ebc4721060bbe719f9bf27479c96eaebf550ae0b
ppranav1123 <111915617+ppranav1123@users.noreply.github.com>
2022-08-30 12:57:13 -0700

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,5 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-import java.util.ArrayList;
+import java.util.*;
 /**
  * Write a description of class Student here.
  * 
@@ -69,38 +69,47 @@ public abstract class Student extends Actor
         mySeatX=getX();
         mySeatY=getY();
     }
- /**The spin image method essentially chnages the location of the image. 
- It first moves the image up 3 spaces. After that, it will move it 3 spaces 
- to the left and then 3 spaces to the right. After that it will move the 
- center of the screen. While it is moving location, it is spinning to the 
- right until the incrementation is done. 
- By Anit Annadi, Samyutha, Brandon, Vihaan**/
-    public void spinImage(int degrees, int increments){
-        int spin=degrees/increments;
-        int addX = 3;
-        int addY = 0;
-        boolean moveImage = true;
-        for (int i=0; i<=increments; i++){
-            getImage().rotate(spin);
+    
+     /**This method is to develop a short animation that goes around in circles and rotate whilst doing so. 
+       The method takes no parameters. 
+       Developed by Pranav Pabbisetty, Pranav Palagummi, Manasvi Pula, Uttara Rai*/
+    public void animation()
+    {
+        setLocation(0,0); //set original location
+        Greenfoot.delay(10); //set the speed of the animation
+        int rotation=getRotation(); //get initial rotation state of the picture
+        // move right and spin around
+        for (int i=1;i<=9;i++){
+            setLocation(i,0);
+            setRotation(getRotation()+90);
             Greenfoot.delay(10);
-            if (i == 1) {
-                addX=0;
-                addY=3;
-            } else if (i==2){
-                addX=-3;
-                addY=0;
-            } else if (i==3){
-                addX=0;
-                addY=-3;
-            } else if (i>3) {
-                moveImage = false;
-                setLocation(6,7);
-            }
-            
-            if (moveImage) {
-                    setLocation(GetSeatX() + addX,GetSeatY() + addY);
-            }
-            Greenfoot.delay(5);
         }
+        // move back and spin around
+        for (int i=1;i<=5;i++){
+            setLocation(9,i);
+            setRotation(getRotation()+90);
+            Greenfoot.delay(10);
+        }      
+        // move left and spin around
+        for (int i=9;i>=0;i--){
+            setLocation(i,5);
+            setRotation(getRotation()+90);
+            Greenfoot.delay(10);
+        }      
+        // move Forward and spin around
+        for (int i=5;i>=0;i--){
+            setLocation(0,i);
+            setRotation(getRotation()+90);
+            Greenfoot.delay(10);
+        }   
+        
+        for (int i=0;i<=5;i++){
+            setRotation(getRotation()+90);
+        }
+        setRotation(rotation); //set the rotation to the original one that was saved
+        returnToSeat(); //return to seat
+        
     }
-}
+    }
+ 
+
