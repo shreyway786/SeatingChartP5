958bfd455eb55f5217ebc53a9850974a7eabb907
sanjanalluri <72996221+sanjanalluri@users.noreply.github.com>
2022-08-30 13:41:54 -0700

    Add files via upload

---
+++ transparent.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class transparent here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class transparent extends SanjanaAlluri
+{
+    /**
+     * Act - do whatever the transparent wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    public transparent() {
+        setImage("greenimage.jpg");
+    }
+    
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
