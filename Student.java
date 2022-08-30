f7cc44d6a2594504fd514b43a34639a9957588fd
ppranav1123 <111915617+ppranav1123@users.noreply.github.com>
2022-08-30 13:10:45 -0700

    Update Student.java

--- Student.java
+++ Student.java
@@ -102,5 +102,45 @@ public abstract class Student extends Actor
             }
             Greenfoot.delay(5);
         }
+      /**This method is to develop a short animation that goes around in circles and rotate whilst doing so. 
+       The method takes no parameters. 
+       Developed by Pranav Pabbisetty, Pranav Palagummi, Manasvi Pula, Uttara Rai*/
+    public void animation()
+    {
+        setLocation(0,0); //set original location
+        Greenfoot.delay(10); //set the speed of the animation
+        int rotation=getRotation(); //get initial rotation state of the picture
+        // move right and spin around
+        for (int i=1;i<=9;i++){
+            setLocation(i,0);
+            setRotation(getRotation()+90);
+            Greenfoot.delay(10);
+        }
+        // move back and spin around
+        for (int i=1;i<=5;i++){
+            setLocation(9,i);
+            setRotation(getRotation()+90);
+            Greenfoot.delay(10);
+        }      
+        // move left and spin around
+        for (int i=9;i>=0;i--){
+            setLocation(i,5);
+            setRotation(getRotation()+90);
+            Greenfoot.delay(10);
+        }      
+        // move Forward and spin around
+        for (int i=5;i>=0;i--){
+            setLocation(0,i);
+            setRotation(getRotation()+90);
+            Greenfoot.delay(10);
+        }   
+        
+        for (int i=0;i<=5;i++){
+            setRotation(getRotation()+90);
+        }
+        setRotation(rotation); //set the rotation to the original one that was saved
+        returnToSeat(); //return to seat
+        
+    }
     }
 }
