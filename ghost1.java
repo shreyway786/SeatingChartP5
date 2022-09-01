1362da3e3f5a63e5896a5e2efe4fc51288ad6aa3
SohaAhmad1 <79863425+SohaAhmad1@users.noreply.github.com>
2022-09-01 13:01:17 -0700

    Add files via upload
    added soha updated files

---
+++ ghost1.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class ghost1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ghost1 extends SohaAhmad
+{
+    /**
+     * Act - do whatever the ghost1 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    private int num=0;
+    public ghost1() {
+        setImage("clydeghost.jpg");
+    }
+}
+
