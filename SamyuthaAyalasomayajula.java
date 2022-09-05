08324795a5bf6c2a59c62f26505e2b30ba03d8ca
samyutha06 <61264088+samyutha06@users.noreply.github.com>
2022-09-04 20:42:41 -0700

    Update SamyuthaAyalasomayajula.java
    updated code

--- SamyuthaAyalasomayajula.java
+++ SamyuthaAyalasomayajula.java
@@ -45,8 +45,8 @@ public class SamyuthaAyalasomayajula extends Student implements SpecialInterestO
         mySeatY=3;
        // imgFile=firstName.toLowerCase()+ lastName.toLowerCase()+".jpg";
        portraitFile=firstName.toLowerCase()+ lastName.toLowerCase()+".jpg";
-       standingFile=firstName.toLowerCase()+ lastName.toLowerCase()+".jpg";
-        soundFile="kilgoretrout.wav";
+       standingFile=firstName.toLowerCase()+ lastName.toLowerCase()+"-standing.jpg";
+        soundFile="samyuthaayalasomayajula.wav";
         setImage(portraitFile);
         sitting=true;
     }
@@ -66,7 +66,7 @@ public class SamyuthaAyalasomayajula extends Student implements SpecialInterestO
                 getName();
                 sayName(soundFile);
             
-                myHobby("I like to time travel!");
+                myHobby("I love to spend time with my friends and family! My favorite food taco bell and my favorite TV show is Brooklyn 99");
             // Create a "special method for your class and put the call here.  You can twirl your image, resize it, move it around, change transparancy, or a 
             // combination of all of those types of actions, or more. Make sure to save the original image if you manipulate it, so that you can put it back.
             // Call the sitDown() method to move back  to your seat
@@ -97,31 +97,43 @@ public class SamyuthaAyalasomayajula extends Student implements SpecialInterestO
      * You should write your own methods to perform your own animation for your character/avatar.
      */
     public void circleClass(){
-        setLocation(0,0);
-         Greenfoot.delay(10);
-        // move right
+   setLocation(0,0); //set original location
+        Greenfoot.delay(10); //set the speed of the animation
+        int rotation=getRotation(); //get initial rotation state of the picture
+        // move right and spin around
         for (int i=1;i<=9;i++){
             setLocation(i,0);
+            setRotation(getRotation()+90);
             Greenfoot.delay(10);
         }
-        // move back
+        // move back and spin around
         for (int i=1;i<=5;i++){
             setLocation(9,i);
+            setRotation(getRotation()+90);
             Greenfoot.delay(10);
         }      
-         // move left
+        // move left and spin around
         for (int i=9;i>=0;i--){
             setLocation(i,5);
+            setRotation(getRotation()+90);
             Greenfoot.delay(10);
         }      
-              // move Forward
+        // move Forward and spin around
         for (int i=5;i>=0;i--){
             setLocation(0,i);
+            setRotation(getRotation()+90);
             Greenfoot.delay(10);
         }   
-           Greenfoot.delay(20);
-           returnToSeat();
+        
+        for (int i=0;i<=5;i++){
+            setRotation(getRotation()+90);
+        }
+        setRotation(rotation); 
+        returnToSeat(); 
+        
     }
+
+
      /**
      * myHobby is one of the interfaces provided.  
      * An interface is just a contract for the methods that you will implement in your code.  The College Board no longer
